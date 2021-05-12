94605cc65e807b3c59d27d2e9481609e4bf1d3d2
Nadir Hatem <nadirhatem@oulook.fr>
2021-05-12 12:56:26 +0100

    Décodage du 3e texte

--- Texte 1.py
+++ Texte 1.py
@@ -17,6 +17,8 @@ def decalage(lettre_message):
 def dec_texte(texte):
     texte_code = ""
     t= 0
+    taille = longueur_cle(texte)
+    print(taille)
     while len(texte_code) < len(texte):
       texte_code += decalage(texte[t])
       t = t + 1
@@ -27,6 +29,32 @@ def chiffre():
     resultat.delete(0, tk.END)
     resultat.insert(0, dec_texte(entree_texte.get()))
 
+def ic(texte):
+    chaine = que_des_majuscules(texte,0)
+    frequences = [0]*26
+    n = len(chaine)
+    for c in chaine:
+        frequences[ord(c)-65] += 1
+    indice = 0.0
+    for ni in frequences:
+        indice += ni*(ni-1)
+    return indice/(n*(n-1))
+
+def longueur_cle(texte):
+    # devine la longueur de la cle avec l'indice de coincidence
+    seuil = 0.06
+    ok = False
+    k = 0
+    while not ok and k<20:
+        partiel = ""
+        k += 1
+        j = 0
+        while j < len(texte):
+            partiel += texte[j]
+            j += k
+        ok = ic(partiel)>seuil
+    return k
+    print (k)
 
 racine=tk.Tk()
 racine.title("Cryptographie")
@@ -39,6 +67,9 @@ label_texte.grid(row = 0, column = 1)
 
 bouton_coder=tk.Button(racine, text="Chiffrer texte",fg="black", width=15, command=chiffre)
 bouton_coder.grid(row=2, column=0)
+
+jj=tk.Button(racine, text="Chiffrer texte",fg="black", width=15, command=longueur_cle)
+jj.grid(row=3, column=5)
 
 
 resultat=tk.Entry(racine,width = 50, font = ("helvetica", "20"))
---
+++ Texte 3.py
@@ -0,0 +1,101 @@
+import tkinter as tk
+
+
+def decalage(lettre_message, lettre_cle):
+    a = ord(lettre_message)
+    c = ord(lettre_cle)
+    b = 0
+    d = 0
+    r = 0
+    p = 0
+    if (a > 96 and 123 > a):
+        al = [97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]
+        for i in al :
+            if i==a :
+                b=al.index(i)
+                print(b)
+        for i in al :
+            if i==c :
+                d = al.index(i)
+                print(d)
+        r = b + d
+        if r > 26 :
+            r = r // 26
+        print (r)
+        print(al[r])
+        p = al[r]
+        print (p)
+        print(chr(p))
+        return chr(p)
+    else:
+        return chr(ord(lettre_message) + 0)
+
+
+def dec_texte(texte, cle):
+    texte_code = ""
+    t, c = 0, 0
+    while len(texte_code) < len(texte):
+        texte_code += decalage(texte[t], cle[c])
+        t, c = t + 1, c + 1
+        if c == len(cle):
+            c = 0
+    return texte_code
+
+
+def chiffre():
+    resultat.delete(0, tk.END)
+    if entree_texte.get() == "" or entree_cle.get() == "":
+        resultat.insert(0, "Il manque quelque chose en entrée :/")
+    resultat.insert(0, dec_texte(entree_texte.get(), entree_cle.get()))
+
+
+def dechiffrement(texte_a_decoder, cle):
+    texte_decode = ""
+    t, c = 0, 0
+    while len(texte_decode) < len(texte_a_decoder):
+        texte_decode += decalage(texte_a_decoder[t], chr(256 - ord(cle[c])))
+        t, c = t + 1, c + 1
+        if c == len(cle):
+            c = 0
+    return texte_decode
+
+
+def dechiffre():
+    if entree_texte.get() == "" or entree_cle.get() == "":
+        resultat.insert(0, "Il manque quelque chose en entrée :/")
+    else:
+        label_res.config(text=dechiffrement(resultat.get(), entree_cle.get()))
+
+
+def chiffre_xor(lettre_message, lettre_cle):
+    return chr(ord(lettre_message) ^ ord(lettre_cle))
+
+
+racine = tk.Tk()
+racine.title("Cryptographie")
+
+entree_texte = tk.Entry(racine, width=50, font=("helvetica", "20"))
+entree_texte.grid(row=0, column=0)
+
+entree_cle = tk.Entry(racine, width=50, font=("helvetica", "20"))
+entree_cle.grid(row=1, column=0)
+
+label_texte = tk.Label(racine, font=("helvetica", "20"), text="Entrer le message ici.")
+label_texte.grid(row=0, column=1)
+
+label_cle = tk.Label(racine, font=("helvetica", "20"), text="Entrer la clé ici.")
+label_cle.grid(row=1, column=1)
+
+bouton_coder = tk.Button(racine, text="Chiffrer texte", fg="black", width=15, command=chiffre)
+bouton_coder.grid(row=2, column=0)
+
+bouton_decoder = tk.Button(racine, text="Déchiffrer texte", fg="black", width=15, command=dechiffre)
+bouton_decoder.grid(row=2, column=1)
+
+resultat = tk.Entry(racine, width=50, font=("helvetica", "20"))
+resultat.grid(row=3, column=0)
+
+label_res = tk.Label(racine, font=("helvetica", "20"), text="Déchiffement ici.")
+label_res.grid(row=3, column=1)
+
+racine.mainloop()
